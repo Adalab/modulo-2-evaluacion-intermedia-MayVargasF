7440493befe50d9d1ee4e4ed0851b8e967727a32
MayVargasF <maytevargasfelix@gmail.com>
2022-07-06 08:56:15 +0200

    html and variables

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Escuchar evento click en el botón
+//F generar random number
+//comparar input con random number
+//si es mayor, poner mensaje de mayor
+//si es menor, poner mensaje menor
+//si es igual, poner mensaje ganadora
+
+const inputNumber = document.querySelector('.js_input');
+const inputBtn = document.querySelector('.js_btn');
+const clue = document.querySelector('.js_clue');
+const attempts = document.querySelector('.js_attempts');
+
